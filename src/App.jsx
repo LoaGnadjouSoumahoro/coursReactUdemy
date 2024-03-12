e80e4d1a69dda572521787b1b574a588ead9b825
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-03-12 14:46:57 +0100

    fin de l'exercice Hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Hello } from "./Hello";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Hello to="Julia" from="Paul" bangs={4} />
+      <Hello to="Dave" />
+    </>
+  );
 }
 
 export default App;
